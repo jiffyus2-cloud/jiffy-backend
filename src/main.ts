3f316b512d7bcff077e4218b349c25731eb39965
jiffyus2-cloud <jiffyus2@gmail.com>
2026-03-03 14:58:26 -0500

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  // Escucha en el puerto definido por Cloud Run o el 8080 por defecto
+  await app.listen(process.env.PORT || 8080);
+}
+bootstrap();
