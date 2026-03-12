6f6f1e43d245ddac495b437cecc093747cec0f24
lucypd56-design <lucypd56@gmail.com>
2026-03-12 17:36:44 -0500

    fix: configuracion cors explicita para google cloud

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // <-- ¡Esta línea es vital!
   
-  // El '0.0.0.0' es OBLIGATORIO para que Cloud Run pueda entrar al contenedor
+  // Configuración CORS a prueba de balas para Google Cloud
+  app.enableCors({
+    origin: true, // Permite que cualquier frontend (incluyendo localhost) se conecte
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  // Escuchando en el puerto dinámico con 0.0.0.0 para aceptar tráfico externo
   await app.listen(process.env.PORT || 8080, '0.0.0.0');
 }
 bootstrap();
