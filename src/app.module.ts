0690de0f8c22505572ebae2259f81e765ae93fd1
lucypd56-design <lucypd56@gmail.com>
2026-03-19 21:56:17 -0500

    fix:IA tools

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { StripeModule } from './stripe/stripe.module';
+import { AiModule } from './ai/ai.module'; // <-- 1. Importamos el nuevo módulo de IA
 
 @Module({
-  imports: [StripeModule],
+  imports: [
+    StripeModule, 
+    AiModule // <-- 2. Lo registramos en la aplicación
+  ],
   controllers: [AppController],
   providers: [],
 })
